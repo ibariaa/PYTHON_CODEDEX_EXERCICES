7543b2c9e8fc336c3e0435bf47470757798a7342
Elaia Ibarboure <elaia.ibarboure@epitech.eu>
2025-01-09 15:00:24 +0100

    [ADD] detention.py

---
+++ Loops/detention.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Suppose you got detention and the teacher wants you to write "I will not use Snapchat in class" on the whiteboard 100 times.
+
+# Create a detention.py program that does this using code.
+
+# (This is where we begin to see the true power of computing. What takes humans hours can take a computer microseconds. 🤯)
+
+for i in range(100):
+    print("I will not use Snapchat in class")
